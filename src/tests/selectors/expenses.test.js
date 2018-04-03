811ce33cae34721fe10b0f4de19f9e876811dc0d
Kiyo <menager.kiyoaki@gmail.com>
2018-04-03 13:33:09 +0200

    Extract expenses fixture in it's own folder/file

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,29 +1,7 @@
 import moment from 'moment';
 import selectExpense from '../../selectors/expenses';
+import expenses from '../fixtures/expenses';
 
-const expenses = [
-  {
-    id: '1',
-    description: 'dog',
-    note: '',
-    amount: 666,
-    createdAt: 0
-  },
-  {
-    id: '2',
-    description: 'Rent',
-    note: '',
-    amount: 109500,
-    createdAt: moment(0).subtract(4, 'days').valueOf()
-  },
-  {
-    id: '3',
-    description: 'Internet',
-    note: '',
-    amount: 4500,
-    createdAt: moment(0).add(4, 'days').valueOf()
-  }
-];
 
 test('should filter by text value', () => {
   const filters = {
