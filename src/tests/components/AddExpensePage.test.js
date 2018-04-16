56379a4abd1c43d3764e1c3b6bae96cd276bc305
Kiyo <menager.kiyoaki@gmail.com>
2018-04-16 21:16:03 +0200

    Update AddExpense feat to write to database
    App:
    * Add startAddExpense action generator
      Triggers persistence then update store
    * Call startAddExpense action generator in AddExpensePage
    
    Tests:
    * Update AddExpensePage.Test
    * Update actions Expenses.test asserting data persisted in both store
      and database.

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -3,14 +3,14 @@ import { shallow } from 'enzyme';
 import { AddExpensePage } from '../../components/AddExpensePage';
 import expenses from '../fixtures/expenses';
 
-let addExpenseSpy, historySpy, wrapper;
+let startAddExpenseSpy, historySpy, wrapper;
 
 beforeEach(() => {
-  addExpenseSpy = jest.fn();
+  startAddExpenseSpy = jest.fn();
   historySpy = { push: jest.fn() };
   wrapper = shallow(
     <AddExpensePage
-      addExpense={addExpenseSpy}
+      startAddExpense={startAddExpenseSpy}
       history={historySpy}
     />);
 });
@@ -23,5 +23,5 @@ test('should handle onSubmit', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
 
   expect(historySpy.push).toHaveBeenLastCalledWith('/');
-  expect(addExpenseSpy).toHaveBeenLastCalledWith(expenses[1]);
+  expect(startAddExpenseSpy).toHaveBeenLastCalledWith(expenses[1]);
 });
