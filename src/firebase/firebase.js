582805b496f78732b3226b3630fc853247e5fa00
Kiyo <menager.kiyoaki@gmail.com>
2018-04-16 20:15:25 +0200

    SetUp firebase
    * Install firebase
    * Install redux-thunk
    * Configure redux store to use middleware redux-thunk

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+import moment from 'moment';
+
+const config = {
+  apiKey: "AIzaSyAUi82cHuHWwEh9Aw2YpdIcz4Nvsy4hbqI",
+  authDomain: "expensify-5d3c4.firebaseapp.com",
+  databaseURL: "https://expensify-5d3c4.firebaseio.com",
+  projectId: "expensify-5d3c4",
+  storageBucket: "expensify-5d3c4.appspot.com",
+  messagingSenderId: "790160790920"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default};
