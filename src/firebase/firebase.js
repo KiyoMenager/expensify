48a0db7525f9de4d4166bdea9f6a3793225b4acc
Kiyo <menager.kiyoaki@gmail.com>
2018-04-16 22:03:02 +0200

    SeTup firebase Env vars for test and development

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 import moment from 'moment';
 
 const config = {
-  apiKey: "AIzaSyAUi82cHuHWwEh9Aw2YpdIcz4Nvsy4hbqI",
-  authDomain: "expensify-5d3c4.firebaseapp.com",
-  databaseURL: "https://expensify-5d3c4.firebaseio.com",
-  projectId: "expensify-5d3c4",
-  storageBucket: "expensify-5d3c4.appspot.com",
-  messagingSenderId: "790160790920"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
